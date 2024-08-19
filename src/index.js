8e108036dbf930da2bbb7600f0a35571dc01efdc
Jake <bajentingjake@gmail.com>
2024-08-19 19:11:51 +0800

    added an image in public

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const Title = () =>{
   return <p className="Title">Atomic Habits</p>;
 }
 const Img = () => {
-  return <img src="https://m.media-amazon.com/images/I/81YkqyaFVEL._SY466_.jpg" alt="Atomic Hobits"></img>;
+  return <img src="./images/atomic_habits.jpg" alt="Atomic Hobits"></img>;
 };
 const Author = () => {
   return <p className="Author">James Clear</p>;
